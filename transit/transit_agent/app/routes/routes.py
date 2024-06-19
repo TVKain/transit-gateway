b19796ae90d927ce53f00731cede43196d9a2ffb
TVKain <tvkain.it@gmail.com>
2024-06-19 01:43:55 +0000

    add: health route

--- transit/transit_agent/app/routes/routes.py
+++ transit/transit_agent/app/routes/routes.py
@@ -8,8 +8,11 @@ from app.routes.interfaces.interfaces import router as interfaces_router
 
 from app.routes.routings.routings import router as routings_router
 
+from app.routes.health.health import router as health_router
+
 api = APIRouter()
 
 api.include_router(configuration_router)
 api.include_router(interfaces_router)
 api.include_router(routings_router)
+api.include_router(health_router)
