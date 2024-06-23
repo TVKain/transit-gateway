e874d29015d927ee383ca7b1ac24410f6822842b
TVKain <tvkain.it@gmail.com>
2024-06-23 15:49:57 +0000

    add: tunnel router api

--- transit/transit_agent/app/routes/routes.py
+++ transit/transit_agent/app/routes/routes.py
@@ -7,9 +7,12 @@ from app.routes.routings.routings import router as routings_router
 
 from app.routes.health.health import router as health_router
 
+from app.routes.tunnels.tunnels import router as tunnels_router
+
 api = APIRouter()
 
 
 api.include_router(interfaces_router)
 api.include_router(routings_router)
 api.include_router(health_router)
+api.include_router(tunnels_router)
